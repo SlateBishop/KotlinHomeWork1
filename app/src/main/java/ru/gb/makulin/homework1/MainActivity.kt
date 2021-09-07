1630a87da6373134794700ddff5455cf24d032c1
Despair <p.a.makulin@gmail.com>
2021-09-07 18:02:45 +0300

    Реализовал обработку кнопки

--- app/src/main/java/ru/gb/makulin/homework1/MainActivity.kt
+++ app/src/main/java/ru/gb/makulin/homework1/MainActivity.kt
@@ -1,11 +1,31 @@
 package ru.gb.makulin.homework1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initButton()
+        setOnClickListener()
+    }
+
+    private fun initButton() {
+        button = findViewById(R.id.test_button)
+    }
+
+    private fun setOnClickListener() {
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(applicationContext, "Работает", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
